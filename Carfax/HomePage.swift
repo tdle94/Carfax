afe6064ec15d68c39468049584cbe4e8534e2178
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-24 21:08:50 -0400

    rename to HomePage

---
+++ Carfax/HomePage.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Carfax
+//
+//  Created by Tuyen Le on 6/24/21.
+//
+
+import UIKit
+
+class HomePage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
