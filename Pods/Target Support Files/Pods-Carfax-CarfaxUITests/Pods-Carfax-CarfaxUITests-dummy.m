1d925efc311f32ee514f894dfb941f998de066f1
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-26 10:49:47 -0400

    add podfile

---
+++ Pods/Target Support Files/Pods-Carfax-CarfaxUITests/Pods-Carfax-CarfaxUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carfax_CarfaxUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Carfax_CarfaxUITests
+@end
---
+++ Pods/Target Support Files/Pods-CarfaxTests/Pods-CarfaxTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarfaxTests : NSObject
+@end
+@implementation PodsDummy_Pods_CarfaxTests
+@end
