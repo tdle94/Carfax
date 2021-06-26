5a7feedb67fca203616e87b82a7e3328cbb876dc
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-26 10:48:47 -0400

    add view model

--- Carfax/HomePageViewModel.swift
+++ Carfax/HomePageViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RxSwift
+
+class HomePageViewModel {
+    private let network = CarFaxNetwork()
+    
+    var carfaxVehicleListing: PublishSubject<[Vehicle]> = PublishSubject()
+    
+    init() {
+        getVehicles()
+    }
+    
+    func getVehicles() {
+        network.getVehicles { result in
+            switch result {
+            case .success(let carfaxResult):
+                self.carfaxVehicleListing.onNext(carfaxResult.listings)
+            case .failure(let error):
+                self.carfaxVehicleListing.onError(error)
+            }
+        }
+    }
+}
