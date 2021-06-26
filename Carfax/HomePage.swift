270da6545251104d024d34481b6dfda116f36643
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-26 10:49:14 -0400

    set up binding to tableview

--- Carfax/HomePage.swift
+++ Carfax/HomePage.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import RxDataSources
+import RxSwift
 
 class HomePage: UIViewController {
     
+    let viewModel = HomePageViewModel()
+    
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var vehicleTableView: UITableView! {
+        didSet {
+            vehicleTableView.register(UINib(nibName: String(describing: VehicleTableViewCell.self), bundle: nil),
+                                      forCellReuseIdentifier: "VehicleTableViewCell")
+            vehicleTableView.tableFooterView = UIView()
+            vehicleTableView.separatorInset.left = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bindUI()
+    }
+    
+    func bindUI() {
+        viewModel
+            .carfaxVehicleListing
+            .bind(to: vehicleTableView.rx.items(cellIdentifier: "VehicleTableViewCell")) { index, model, cell in
+                let vehicleCell = cell as? VehicleTableViewCell
+                vehicleCell?.configureVehicle(model)
+            }
+            .disposed(by: disposeBag)
+
     }
 }
-
