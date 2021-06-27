455e215ef2d965d66483be9fc7312cfdfb173d12
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-26 20:58:57 -0400

    add vehicle listing background view

--- Carfax/CustomViews/VehicleTableBackgroundView.swift
+++ Carfax/CustomViews/VehicleTableBackgroundView.swift
@@ -5,4 +5,12 @@
 //  Created by Tuyen Le on 6/26/21.
 //
 
-import Foundation
+import UIKit
+
+class VehicleTableBackgroundView: UIView {
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var tryAgainButton: UIButton!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+}
