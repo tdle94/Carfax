af8e3d79d9ef1ab9aef161c2d1a31ae37eb3033f
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-26 20:58:22 -0400

    add Vehicle View Model

--- Carfax/View Models/VehicleViewModel.swift
+++ Carfax/View Models/VehicleViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class VehicleViewModel {
+    var vehicle: Vehicle
+    
+    var formattedPhoneNumber: String {
+        return vehicle.dealer.phone.replacingOccurrences(of: "(\\d{3})(\\d{3})(\\d+)", with: "($1) $2-$3", options: .regularExpression, range: nil)
+    }
+    
+    init(vehicle: Vehicle) {
+        self.vehicle = vehicle
+    }
+    
+    func callDealerPhoneNumber(_ phoneNumber: String?) {
+        guard let number = phoneNumber else { return }
+        if let url = URL(string: "tel://\(number)"), UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+}
