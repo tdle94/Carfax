d2cd009ac6b729a8c1f12be28d4424088f0dcbb0
Tuyen Le <electro@Tuyens-iMac.local>
2021-06-27 12:49:31 -0400

    add test scheme

--- CarfaxTests/CarFaxSessionMock.swift
+++ CarfaxTests/CarFaxSessionMock.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class CarFaxSessionMock: CarFaxSession {
+    
+    var data: Data?
+    var error: Error?
+    
+    func fetch<T>(url: URL, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable {
+        if let data = self.data {
+            let decodeObj = try! JSONDecoder().decode(CarfaxListings.self, from: data)
+            completion(.success(decodeObj as! T))
+        }
+    }
+}
